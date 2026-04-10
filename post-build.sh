9e1bdab90a68fa7937db38e1d1623f40833faf7e
monjit-TAM <monjit@tam.com>
2026-04-10 14:07:42 +0000

    Add: Broker Integration Guide HTML at /api/broker-guide

--- post-build.sh
+++ post-build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp /var/www/alphamarket/server/broker-swagger.json /var/www/alphamarket/dist/public/broker-swagger.json 2>/dev/null
-echo "Post-build: broker swagger copied"
+cp /var/www/alphamarket/server/broker-guide.html /var/www/alphamarket/dist/public/broker-guide.html 2>/dev/null
+echo "Post-build: broker assets copied"
